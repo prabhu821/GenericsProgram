439eae0423746abc40fc8955f56738e82e376bc7
rishiraj007 <rishiraj@mounttalent.com>
2023-03-01 18:08:36 +0530

    [Prabhu] Add: Max Float

--- ProgramGenerics/FindMaxNumber.cs
+++ ProgramGenerics/FindMaxNumber.cs
@@ -24,4 +24,22 @@ public class FindMaxNumber
         }
         return a;
     }
+
+    //MAX FLOAT NUMBER
+    public static double MaxFloatNumber(double a, double b, double c)
+    {
+        if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+        {
+            return a;
+        }
+        if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+        {
+            return b;
+        }
+        if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
+        {
+            return c;
+        }
+        return a;
+    }
 }
--- ProgramGenerics/Program.cs
+++ ProgramGenerics/Program.cs
@@ -7,5 +7,8 @@ public class Program
         Console.WriteLine("Generics Program");
         int output = FindMaxNumber.MaxIntegerNumber(10, 3, 5);
         Console.WriteLine(output);
+
+        double output1 = FindMaxNumber.MaxFloatNumber(10.2, 3.3, 5.4);
+        Console.WriteLine(output1);
     }
 }
