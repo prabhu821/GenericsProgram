c7079927ce247f3dbe75757d4df2112bccd7d8e7
rishiraj007 <rishiraj@mounttalent.com>
2023-03-01 18:30:50 +0530

    [Prabhu] Add: Generic Max Number

---
+++ ProgramGenerics/GenericMaximum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramGenerics;
+
+public class GenericMaximum<T> where T : IComparable
+{
+    public T[] value;
+
+    public GenericMaximum(T[] value)
+    {
+        this.value = value;
+    }
+
+    public T[] Sort(T[] values)
+    {
+        Array.Sort(values);
+        return values;
+    }
+
+    public T MaxValue(params T[] values)
+    {
+        var sorted_values = Sort(values);
+        return sorted_values[^1];
+    }
+
+    public T MaxMethod()
+    {
+        var max = MaxValue(this.value);
+        return max;
+    }
+
+    //PRINT MAX VALUE
+    public void PrintMaxValue()
+    {
+        var max = MaxValue(this.value);
+        Console.WriteLine("Maximum value is " + max);
+    }
+}
--- ProgramGenerics/Program.cs
+++ ProgramGenerics/Program.cs
@@ -5,13 +5,43 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Generics Program");
-        int output = FindMaxNumber.MaxIntegerNumber(10, 3, 5);
-        Console.WriteLine(output);
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\nEnter your choice \n1.Max Integer  \n2.Max Float \n3.Max String \n4.Array Max \n5.Exit");
+            Console.WriteLine("\nEnter option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    int output = FindMaxNumber.MaxIntegerNumber(10, 3, 5);
+                    Console.WriteLine(output);
+                    break;
+                case 2:
+                    double output1 = FindMaxNumber.MaxFloatNumber(10.2, 3.3, 5.4);
+                    Console.WriteLine(output1);
+                    break;
+                case 3:
+                    string stringoutput = FindMaxNumber.MaxString("Strawberry", "Apple", "Banana");
+                    Console.WriteLine(stringoutput);
+                    break;
+                case 4:
+                    int[] intArray = { 112, 244, 534, 453 };
+                    GenericMaximum<int> genric = new GenericMaximum<int>(intArray);
+                    genric.PrintMaxValue();
 
-        double output1 = FindMaxNumber.MaxFloatNumber(10.2, 3.3, 5.4);
-        Console.WriteLine(output1);
+                    double[] doubleArray = { 11.2, 24.4, 5.34, 45.3 };
+                    GenericMaximum<double> genricDouble = new GenericMaximum<double>(doubleArray);
+                    genricDouble.PrintMaxValue();
 
-        string stringoutput = FindMaxNumber.MaxString("Strawberry", "Apple", "Banana");
-        Console.WriteLine(stringoutput);
+                    string[] stringArray = { "112", "244", "534", "453" };
+                    GenericMaximum<string> genricString = new GenericMaximum<string>(stringArray);
+                    genricString.PrintMaxValue();
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
