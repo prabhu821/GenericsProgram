e0f8d42b5ca607b07cbb2092b9174b432338537a
rishiraj007 <rishiraj@mounttalent.com>
2023-03-01 18:03:30 +0530

    [Prabhu] Add: Max Integer

---
+++ ProgramGenerics/FindMaxNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramGenerics;
+
+public class FindMaxNumber
+{
+    public static int MaxIntegerNumber(int a, int b, int c)
+    {
+        if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+        {
+            return a;
+        }
+        if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+        {
+            return b;
+        }
+        if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
+        {
+            return c;
+        }
+        return a;
+    }
+}
--- ProgramGenerics/Program.cs
+++ ProgramGenerics/Program.cs
@@ -5,5 +5,7 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Generics Program");
+        int output = FindMaxNumber.MaxIntegerNumber(10, 3, 5);
+        Console.WriteLine(output);
     }
 }
